7195d80cead56cb94f404f1c3d2a97af2ad2a45c
Vengateswaran <vengateswarankannan@gmail.com>
2026-06-29 14:04:59 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, redirect, render_template, request, url_for
-from flask_migrate import Migrate
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from models import User, db
 
 app = Flask(__name__, template_folder=".", static_folder=".", static_url_path="")
@@ -10,23 +8,18 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:vengat%40123@local
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
-migrate = Migrate(app, db)
-
 
 @app.route("/")
 def home():
-    return redirect(url_for("signin"))
-
-
-@app.route("/signin")
-def signin():
-    return render_template("signin.html")
-
+    return redirect(url_for("signup"))
 
 @app.route("/signup")
 def signup():
     return render_template("signup.html")
 
+@app.route("/signin")
+def signin():
+    return render_template("signin.html")
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -38,20 +31,19 @@ def register():
     if password != confirm_password:
         return "Passwords do not match"
 
-    existing_user = User.query.filter_by(email=email).first()
-    if existing_user:
+    if User.query.filter_by(email=email).first():
         return "Email already registered"
 
     user = User(
         name=name,
         email=email,
-        password=generate_password_hash(password),
+        password=generate_password_hash(password)
     )
+
     db.session.add(user)
     db.session.commit()
 
     return redirect(url_for("signin"))
-
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -59,11 +51,11 @@ def login():
     password = request.form.get("password", "")
 
     user = User.query.filter_by(email=email).first()
+
     if user and check_password_hash(user.password, password):
-        return f"Welcome, {user.name}!"
+        return "Login successful"
 
     return "Invalid email or password"
-
 
 if __name__ == "__main__":
     with app.app_context():
